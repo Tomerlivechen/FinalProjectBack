abfe1009231c8a14ca0f3c9b15d3891f93da175c
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-11-03 21:04:38 +0200

    corrected cors

--- FinalProject3/Program.cs
+++ FinalProject3/Program.cs
@@ -43,7 +43,7 @@ namespace FinalProject3
                         "http://localhost:5173",
                         "http://localhost:5174",
                         "https://white-pond-0c2316100.5.azurestaticapps.net",
-                        "https://final-project-front-nu.vercel.app/"
+                        "https://final-project-front-nu.vercel.app"
                         //host
                         ]).AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithExposedHeaders("Authorization");
 
