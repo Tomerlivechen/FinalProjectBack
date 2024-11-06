d78853ec3b2e4dfcea0f85e12ed4978a914a82e4
Dr. Tomer Chen <104824283+Tomerlivechen@users.noreply.github.com>
2024-11-06 15:09:09 +0200

    added app.UseRouting();

--- FinalProject3/Program.cs
+++ FinalProject3/Program.cs
@@ -38,18 +38,19 @@ namespace FinalProject3
             {
                 options.AddPolicy(name: corsPolicy, policy =>
                 {
-                    policy.WithOrigins([
+                    policy.WithOrigins(
                         "http://localhost:3000",
                         "http://localhost:5173",
                         "http://localhost:5174",
                         "https://white-pond-0c2316100.5.azurestaticapps.net",
                         "https://final-project-front-nu.vercel.app","https://finalprojectfront-6dxw.onrender.com"
                         //host
-                        ]).AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithExposedHeaders("Authorization");
+                        ).AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithExposedHeaders("Authorization");
 
                 });
             });
             var app = builder.Build();
+            app.UseRouting();
             app.UseCors(corsPolicy);
 
             // Configure the HTTP request pipeline.
