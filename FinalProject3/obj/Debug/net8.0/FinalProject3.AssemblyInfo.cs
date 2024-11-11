7da16e100091ce02081e8ef6573c371e2728d699
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-11-11 23:12:43 +0200

    run

--- FinalProject3/obj/Debug/net8.0/FinalProject3.AssemblyInfo.cs
+++ FinalProject3/obj/Debug/net8.0/FinalProject3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de39126007a9fb8dc3ec1ea493e56cac169585fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cccbb0ef66cab642e431414e762e12d731bf1db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
